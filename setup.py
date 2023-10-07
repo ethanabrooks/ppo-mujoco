a081bd242d5c287bf56681b23829cbb32d4cc45d
ethanabrooks <ethanabrooks@gmail.com>
2023-10-07 13:07:00 -0400

    remove requirements.txt and setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    name="ppo-mujoco",
-    packages=find_packages(),
-    version="0.0.1",
-    install_requires=["gym", "matplotlib", "pybullet", "torch", "mujoco_py"],
-)
